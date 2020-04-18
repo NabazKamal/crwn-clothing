4ada6faacb822d181a83f6be4a5cca9a3dd3dc13
NabazKamal <nabaz.kamal@gmail.com>
2020-04-18 23:08:28 +0200

    Update styling for hover and scaling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepage.style.scss';
+import Directory from '../../components/directory-item/directory-item.component'
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default HomePage
